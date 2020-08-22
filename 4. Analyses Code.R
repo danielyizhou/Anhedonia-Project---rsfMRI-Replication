e3ec4a0c28d0947207db733dfdb029d0da4b2518
Daniel Zhou <daniel.yi.zhou@gmail.com>
2020-08-22 16:38:28 -0400

    automization progress

--- 4. Analyses Code.R
+++ 4. Analyses Code.R
@@ -29,3 +29,78 @@ for (val in x){
 
 #make sure you are using the "weights" paramter correctly 
 
+
+# Part 1 ------------------------------------------------------------------
+
+
+#some tests to extract model parameters 
+res1 <- gamm4(data4$rsfmri_cor_network.gordon_cingulooperc_network.gordon_cingulooperc ~ Anhedonia + interview_age + sex + Black_ethnicity + Hispanic_ethnicity + Asian_ethnicity + Other_ethnicity,
+              random = ~(1|rel_family_id) + (1|mri_info_device.serial.number),
+              weights = data4$acs_raked_propensity_score,
+              data = data4)
+par(mfrow = c(2,2))       #make 2 by 2 window for plots
+gam.check(res1$gam)  #check's residuals of analysis
+
+summary_variable <- summary(res1$gam)
+summary_variable
+
+summary_variable$se   #gets the standard errors of the model
+summary_variable$p.t  #gets the t values from the model
+summary_variable$p.pv #gets the p-values for the parameters of the model
+summary_variable$p.coeff #gets the coefficients of the fitted model
+summary_variable$r.sq #gets r squared of the model
+
+summary_variable$formula[2] #gets column name (rsfMRI network) 
+  string_colname <- as.character(summary_variable$formula[2]) #converts it to a string
+  colname_Vector <- c(rep(string_colname, 8)) #make a vector to repeat it 8 times 
+
+#create data frame with extracted elements
+my_df1 <- data.frame("Parameters" = row.names(my_df1),
+                    "rsfMRI Network" = colname_Vector, 
+                    "Estimate" = summary_variable$p.coeff, 
+                    "Std. Error" = summary_variable$se,
+                    "t value" = summary_variable$p.t, 
+                    "p-value" = summary_variable$p.pv)
+
+#add asterix symbols to determine level of significance
+my_df1$sig_level <- ifelse(my_df1$p.value <= 0.05 & my_df1$p.value > 0.01, "*",
+                            ifelse(my_df1$p.value <= 0.01 & my_df1$p.value > 0.001, "**",
+                                    ifelse(my_df1$p.value <=0.001, "***", "not significant"))) 
+
+
+# Part 2 ------------------------------------------------------------------
+
+res2 <- gamm4(data4$rsfmri_cor_network.gordon_salience_subcort.aseg_ventraldc.lh ~ Anhedonia + interview_age + sex + Black_ethnicity + Hispanic_ethnicity + Asian_ethnicity + Other_ethnicity,
+              random = ~(1|rel_family_id) + (1|mri_info_device.serial.number),
+              weights = data4$acs_raked_propensity_score,
+              data = data4)
+summary_variable2 <- summary(res2$gam)
+summary_variable2
+
+summary_variable2$p.t  #gets the t values from the model
+summary_variable2$p.pv #gets the p-values for the parameters of the model
+summary_variable2$p.coeff #gets the coefficients of the fitted model
+summary_variable2$r.sq #gets r squared of the model
+
+summary_variable2$formula[2] #gets column name (rsfMRI network) 
+string_colname2 <- as.character(summary_variable2$formula[2]) #converts it to a string
+colname_Vector2 <- c(rep(string_colname2, 8)) #make a vector to repeat it 8 times 
+
+#create data frame with extracted elements
+my_df2 <- data.frame("Parameters" = row.names(my_df2),
+                    "rsfMRI Network" = colname_Vector2, 
+                    "Estimate" = summary_variable2$p.coeff, 
+                    "Std. Error" = summary_variable2$se,
+                    "t value" = summary_variable2$p.t, 
+                    "p-value" = summary_variable2$p.pv)
+
+#add asterix symbols to determine level of significance
+my_df2$sig_level <- ifelse(my_df2$p.value <= 0.05 & my_df2$p.value > 0.01, "*",
+                          ifelse(my_df2$p.value <= 0.01 & my_df2$p.value > 0.001, "**",
+                                 ifelse(my_df2$p.value <=0.001, "***", "not significant"))) 
+
+
+# Joining Data Frames -----------------------------------------------------
+
+df_merge <- merge(my_df1, my_df2, all = TRUE, sort = FALSE)
+  
